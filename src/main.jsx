ee268d3ccae91f12444c06c1b4e8aed6a3ae8f6f
Ian-austen <252906145@qq.com>
2026-06-15 14:50:06 +0800

    debug

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import React from 'react'
-import ReactDom from 'react-dom/client'
-import App from './App.jsx'
-ReactDom.createRoot(document.getElementById('root')).render()
+import ReactDOM from 'react-dom/client'
+import App from '/src/App.jsx' // 尝试把 ./ 改成 /src/
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
